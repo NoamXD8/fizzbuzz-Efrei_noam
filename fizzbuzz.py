346dc116779aff8630c1e40edbca089bfe9b58a7
Noam Boulze <noamboulze@gmail.com>
2025-02-06 12:17:57 +0100

    docstring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,12 @@
 def fizzbuzz(n):
+    '''
+    Cette fonction génère une suite contenant les résultats de FizzBuzz pour les nombres de 1 à n-1. 
+    Voici les règles :
+    - Si le nombre est divisible par 3 et 5, on affiche "FizzBuzz".
+    - Si le nombre est divisible par 3 mais pas par 5, on affiche "Fizz".
+    - Si le nombre est divisible par 5 mais pas par 3, on afffiche "Buzz".
+    - Sinon, on affiche juste le nombre en question.
+    '''
     result = []
     for i in range(1, n):
         if i % 3 == 0 and i % 5 == 0:
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -6,24 +6,36 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from fizzbuzz import fizzbuzz
 
 def test_fizz():
+    '''
+    Test de la fonction fizzbuzz dans les cas où elle doit retourner "Fizz"
+    '''
     assert fizzbuzz(4)[2] == "Fizz"
     assert fizzbuzz(7)[5] == "Fizz"
     assert fizzbuzz(10)[8] == "Fizz"
     assert fizzbuzz(13)[11] == "Fizz"
 
 def test_buzz():
+    '''
+    Test de la fonction fizzbuzz dans les cas où elle doit retourner "Buzz"
+    '''
     assert fizzbuzz(6)[4] == "Buzz"
     assert fizzbuzz(11)[9] == "Buzz"
     assert fizzbuzz(21)[19] == "Buzz"
     assert fizzbuzz(26)[24] == "Buzz"
 
 def test_fizzbuzz():
+    '''
+    Test de la fonction fizzbuzz dans les cas où elle doit retourner "FizzBuzz"
+    '''
     assert fizzbuzz(16)[14] == "FizzBuzz"
     assert fizzbuzz(31)[29] == "FizzBuzz"
     assert fizzbuzz(46)[44] == "FizzBuzz"
     assert fizzbuzz(61)[59] == "FizzBuzz"
 
 def test_numbers():
+    '''
+    Test de la fonction fizzbuzz dans les cas où elle doit retourner un nombre"
+    '''
     assert fizzbuzz(2)[0] == '1'
     assert fizzbuzz(3)[1] == '2'
     assert fizzbuzz(5)[3] == '4'
