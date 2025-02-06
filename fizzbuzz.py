b1ed8b3013605da3bc2d933d713058b4b1b9e892
Noam Boulze <noamboulze@gmail.com>
2025-02-06 10:05:50 +0100

    commit new unit test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,16 @@
-def fizzbuzz():
+def fizzbuzz(n):
     result = []
-    for i in range(1,101):
+    for i in range(1, n):
         if i % 3 == 0 and i % 5 == 0:
             result.append("FizzBuzz")
-            print("FizzBuzz")
+            #print("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz")
+            #print("Fizz")
             result.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz")
+            #print("Buzz")
             result.append("Buzz")
         else:
-            print(i)
+            #print(i)
             result.append(str(i))
     return result
-
-fizzbuzz()
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,20 +5,22 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 from fizzbuzz import fizzbuzz
 
-expected_output = [
-    '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz',
-    '11', 'Fizz', '13', '14', 'FizzBuzz', '16', '17', 'Fizz', '19', 'Buzz',
-    'Fizz', '22', '23', 'Fizz', 'Buzz', '26', 'Fizz', '28', '29', 'FizzBuzz',
-    '31', '32', 'Fizz', '34', 'Buzz', 'Fizz', '37', '38', 'Fizz', 'Buzz',
-    '41', 'Fizz', '43', '44', 'FizzBuzz', '46', '47', 'Fizz', '49', 'Buzz',
-    'Fizz', '52', '53', 'Fizz', 'Buzz', '56', 'Fizz', '58', '59', 'FizzBuzz',
-    '61', '62', 'Fizz', '64', 'Buzz', 'Fizz', '67', '68', 'Fizz', 'Buzz',
-    '71', 'Fizz', '73', '74', 'FizzBuzz', '76', '77', 'Fizz', '79', 'Buzz',
-    'Fizz', '82', '83', 'Fizz', 'Buzz', '86', 'Fizz', '88', '89', 'FizzBuzz',
-    '91', '92', 'Fizz', '94', 'Buzz', 'Fizz', '97', '98', 'Fizz', 'Buzz'
-]
+def test_fizz():
+    assert fizzbuzz(4)[2] == "Fizz"
+    assert fizzbuzz(7)[5] == "Fizz"
+    assert fizzbuzz(10)[8] == "Fizz"
 
+def test_buzz():
+    assert fizzbuzz(6)[4] == "Buzz"
+    assert fizzbuzz(11)[9] == "Buzz"
+    assert fizzbuzz(21)[19] == "Buzz"
 
 def test_fizzbuzz():
-    result = fizzbuzz()
-    assert result == expected_output
+    assert fizzbuzz(16)[14] == "FizzBuzz"
+    assert fizzbuzz(31)[29] == "FizzBuzz"
+    assert fizzbuzz(46)[44] == "FizzBuzz"
+
+def test_numbers():
+    assert fizzbuzz(2)[0] == '1'
+    assert fizzbuzz(3)[1] == '2'
+    assert fizzbuzz(5)[3] == '4'
