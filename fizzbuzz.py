3b7f675756c2990f74342a6fbd159e751254cf64
Noam Boulze <noamboulze@gmail.com>
2025-02-06 09:37:18 +0100

    first try CI/CD

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,18 @@
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
-#test
+def fizzbuzz():
+    result = []
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+            result.append("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+            result.append("Buzz")
+        else:
+            print(i)
+            result.append(str(i))
+    return result
+
+fizzbuzz()
